879eca6bcf9391e7e5563005a185badfbfead08c
Dohyeop Lim <dhlim0817@gmail.com>
2021-11-07 01:43:00 +0900

    Fix: Remove link label of Tabbar component

--- components/Tabbar.js
+++ components/Tabbar.js
@@ -2,17 +2,16 @@ import Link from "next/link";
 import { MdChat, MdHomeFilled, MdPeopleAlt } from "react-icons/md";
 
 const Tabbar = ({ pathname }) => {
-  const TabbarLink = ({ label, icon, href }) => {
+  const TabbarLink = ({ icon, href }) => {
     return (
       <Link href={href}>
         <a>
           <div
-            className={`flex flex-col justify-center items-center px-8 py-1 z-50 ${
+            className={`flex flex-col relative justify-center items-center no-underline px-3 py-2 z-50 text-2xl ${
               pathname === href && "border-b-2 border-blue-500 text-blue-500"
             }`}
           >
             {icon}
-            <p>{label}</p>
           </div>
         </a>
       </Link>
@@ -21,11 +20,11 @@ const Tabbar = ({ pathname }) => {
 
   return (
     <nav className="flex fixed overflow-hidden bottom-0 z-50 justify-between items-center w-full h-20 pb-14 pt-8 px-10 bg-gray-50">
-      <TabbarLink label="홈" icon={<MdHomeFilled />} href="/" />
+      <TabbarLink icon={<MdHomeFilled />} href="/" />
 
-      <TabbarLink label="채팅" icon={<MdChat />} href="/chat" />
+      <TabbarLink icon={<MdChat />} href="/chat" />
 
-      <TabbarLink label="만든 사람" icon={<MdPeopleAlt />} href="/credit" />
+      <TabbarLink icon={<MdPeopleAlt />} href="/credit" />
     </nav>
   );
 };
